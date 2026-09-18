2ca97cd45a4d0000ac4dc749c49a507c86a2c70b
Shreyas Kulkarni <135884938+Shreyaskulkarni56@users.noreply.github.com>
2026-09-18 23:32:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,16 +12,38 @@ function convertToRoman(num) {
   //your code here
 	let result ="";
 
-	for(const [index,symbol of object.values(obj)){
-		while(num >=index){
-			result+=symbol;
-			num-=index;
-		}
-	}
-return result;
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+	const values = Object.values(obj);
 
+  // 2. Safely loop through the values array by index
+  for (let i = 0; i < values.length; i++) {
+    const [symbol, value] = values[i];
+
+    // Handle standard addition (e.g., 30 -> XXX, 5 -> V)
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+
+    // 3. Handle subtractive cases (like 4, 9, 40, 90, 400, 900) dynamically
+    // We check if subtracting a future smaller unit creates a valid prefix rule
+    if (i < values.length - 1) {
+      // For 'V' (5) or 'D' (500), the subtractive prefix modifier is 2 steps away ('I' or 'C')
+      // For 'X' (10) or 'C' (100) or 'M' (1000), the prefix modifier is 2 steps away ('I', 'X', 'C')
+      const nextIndex = (i % 2 === 0) ? i + 2 : i + 1; 
+      
+      if (nextIndex < values.length) {
+        const [subSymbol, subValue] = values[nextIndex];
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+	   if (num>=value-subSymbol){
+		   result+=subSymbol + symbol ;
+		   num-=(value -subSymbol);
+	   }
+	  }
+	   }
+	  }
+	return result;
+}
+	  
 console.log(convertToRoman(36));
 
 
